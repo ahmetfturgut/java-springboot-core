291bbfd07e3e55d3d17f2852e72884dd880f1b82
Ahmet Faruk Turgut <ahmetfturgut@gmail.com>
2024-04-01 03:53:03 +0300

    bugfixed

--- src/main/java/com/javaspringboot/javaspringbootcore/app/user/controller/UserController.java
+++ src/main/java/com/javaspringboot/javaspringbootcore/app/user/controller/UserController.java
@@ -14,8 +14,6 @@ import com.javaspringboot.javaspringbootcore.core.service.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
@@ -83,8 +81,9 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public ApiResponse<User> getUser(@PathVariable("id") Long id) {
-        User user = userService.getUser(id);
+    public ApiResponse<User> getUser(@PathVariable("id") Long userId) {
+
+        User user = userService.getUser(userId);
         return new ApiResponse<User>(user);
     }
 
